9d93b4a604893e89ebe7bcda13d5b886a99cde61
Erik Ring-Walters <erik.ring.walters@gmail.com>
2018-10-03 11:36:53 -0700

    Added sidebar

---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss']
+})
+export class NavBarComponent implements OnInit {
+
+  currentUrl: string;
+
+  constructor(private router: Router) {
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((_: NavigationEnd) => { this.currentUrl = _.url; });
+
+  }
+
+  ngOnInit() {
+  }
+
+}
