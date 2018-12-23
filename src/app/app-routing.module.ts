a65a6fa4f3d58d93caa5afe6a75a13f08b849e30
Erik Ring-Walters <erik.ring.walters@gmail.com>
2018-12-23 02:59:11 -0800

    fixed home routing and added images

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   {
     path: 'about',
     component: AboutComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
   },
 ];
 
