7a092bc5da1d207e952a86c1285a2f2a62f99cf9
Erik Ring-Walters <erik.ring.walters@gmail.com>
2019-01-05 19:33:17 -0800

    added class for quilt tile

---
+++ src/app/tile.ts
@@ -0,0 +1,16 @@
+class Tile {
+  id: number;
+  name: string;
+  imagePath: string;
+  url: string;
+
+  constructor(name: string, path: string) {
+      this.name = name;
+      this.imagePath = path;
+  }
+  // greet() {
+  //     return "Hello, " + this.greeting;
+  // }
+}
+
+//let greeter = new Greeter("world");
