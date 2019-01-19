2f59171c28048485c9b255bc85d21e34b62e8abf
Erik Ring-Walters <erik.ring.walters@gmail.com>
2019-01-19 13:13:17 -0800

    heroku configs again

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '../dist/uroc-app'));
+app.use(express.static(__dirname + './dist/uroc-app'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'../dist/uroc-app/index.html'));
+res.sendFile(path.join(__dirname+'./dist/uroc-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
