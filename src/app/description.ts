7adcb8b9026745ffc1a9d553279f6ea2cf07665d
Erik Ring-Walters <erik.ring.walters@gmail.com>
2019-03-10 20:57:53 -0700

    added description class (not yet implemented)

---
+++ src/app/description.ts
@@ -0,0 +1,17 @@
+export class Description {
+  subtitle1: string;
+  paragraph1: string;
+  subtitle2: string;
+  paragraph2: string;
+  subtitle3: string;
+  paragraph3: string;
+
+  constructor(id: number, path: string) {
+      this.subtitle1 = this.subtitle1;
+      this.paragraph1 = this.paragraph1;
+      this.subtitle2 = this.subtitle2;
+      this.paragraph2 = this.paragraph2;
+      this.subtitle3 = this.subtitle3;
+      this.paragraph3 = this.paragraph3;
+  }
+}
