e5d3309802d1c56322aacb387586f15122dae0c1
Erik Ring-Walters <erik.ring.walters@gmail.com>
2019-04-22 14:58:36 -0700

    mat tool tip not working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { MatTooltipModule } from '@angular/material/tooltip';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DetailsComponent } from './details/details.component';
+
 import { CookieService } from 'angular2-cookie/services/cookies.service';
 import { TileService } from './tile.service';
 
@@ -24,6 +24,7 @@ import { TileService } from './tile.service';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    // MatTooltipModule
   ],
   providers: [ CookieService, TileService ],
   bootstrap: [AppComponent]
